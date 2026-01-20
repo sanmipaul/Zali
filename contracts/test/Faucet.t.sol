847079c2fd5c18e61469d8869997b533abe69194
sanmipaul <sanmipaul@example.com>
2026-01-20 11:41:57 +0100

    Update Faucet.t.sol to import MockERC20 from separate file

--- contracts/test/Faucet.t.sol
+++ contracts/test/Faucet.t.sol
@@ -3,13 +3,7 @@ pragma solidity ^0.8.20;
 
 import "forge-std/Test.sol";
 import "../src/Faucet.sol";
-import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
-
-contract MockERC20 is ERC20 {
-    constructor() ERC20("Mock cUSD", "mcUSD") {
-        _mint(msg.sender, 1000000 * 10**18);
-    }
-}
+import "./MockERC20.sol";
 
 contract FaucetTest is Test {
     Faucet public faucet;
