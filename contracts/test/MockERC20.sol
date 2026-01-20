ccf1583ddcc6bea8134bd44aa63517d46c7b9146
sanmipaul <sanmipaul@example.com>
2026-01-20 11:46:27 +0100

    Add comment to MockERC20 contract

--- contracts/test/MockERC20.sol
+++ contracts/test/MockERC20.sol
@@ -3,6 +3,7 @@ pragma solidity ^0.8.20;
 
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
+// Mock ERC20 token for testing purposes
 contract MockERC20 is ERC20 {
     constructor() ERC20("Mock cUSD", "mcUSD") {
         _mint(msg.sender, 1000000 * 10**18);
