75ef73b1b7db995e6d6f6604d39df9568cec949d
sanmipaul <sanmipaul@example.com>
2026-01-20 11:42:41 +0100

    Update TriviaGame.t.sol to import MockERC20 from separate file

--- contracts/test/TriviaGame.t.sol
+++ contracts/test/TriviaGame.t.sol
@@ -3,7 +3,7 @@ pragma solidity ^0.8.20;
 
 import "forge-std/Test.sol";
 import "../src/TriviaGame.sol";
-import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "./MockERC20.sol";
 
 contract TriviaGameTest is Test {
     TriviaGame public triviaGame;
@@ -137,11 +137,5 @@ contract TriviaGameTest is Test {
         
         // Check game state
         assertEq(uint256(triviaGame.getGameState(1)), 3); // GameState.Cancelled
-    }
-}
-
-contract MockERC20 is ERC20 {
-    constructor() ERC20("Mock cUSD", "mcUSD") {
-        _mint(msg.sender, 1000000 * 10**18);
     }
 }
